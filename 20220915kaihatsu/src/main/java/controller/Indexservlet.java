3f29c0fdc41ccf45689fa8788a21cc370646f4fa
takedayu <yuta.takeda@koki-holdings.co.jp>
2022-11-21 15:44:39 +0900

    branch

--- 20220915kaihatsu/src/main/java/controller/Indexservlet.java
+++ 20220915kaihatsu/src/main/java/controller/Indexservlet.java
@@ -8,14 +8,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @WebServlet("/Index")
 public class Indexservlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
-		HttpSession session = request.getSession(true);
+//		HttpSession session = request.getSession(true);
 		RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/lib/view/index.jsp");
 		rd.forward(request, response);
 
